3f14d83d1702d092468ae0a4f26d92f290e1bc3b
Alexis Folgar Martinez <Alexis.folgar@outlook.es>
2022-10-12 22:14:14 -0600

    Arreglo de Vistas Catalogo Compania y formulario servicio de prevencion

--- Bomberos/Models/BomberoContext.cs
+++ Bomberos/Models/BomberoContext.cs
@@ -36,7 +36,7 @@ namespace Bomberos.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Bombero;user=sa;password=LDesarrollo#4;Trusted_Connection=false;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=ALEXISPC\\SQLEXPRESS;Database=Bombero;user=sa;password=LDesarrollo#4;Trusted_Connection=false;MultipleActiveResultSets=true");
             }
         }
 
