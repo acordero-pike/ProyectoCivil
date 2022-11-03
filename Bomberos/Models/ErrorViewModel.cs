7bfc9db50bd44f2734db51b313ee3c14e83f933c
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2022-11-03 16:33:25 -0600

    Delete Bomberos directory

--- Bomberos/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bomberos.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
